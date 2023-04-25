8b435d9560b23e949156474ce59add7fc5566680
fefe94 <federico.ferreiro@pedidosya.com>
2023-04-24 21:30:22 -0300

    imgs routes

--- src/components/Segmentos/Segmento.jsx
+++ src/components/Segmentos/Segmento.jsx
@@ -3,20 +3,20 @@ import { useParams } from 'react-router-dom';
 import { AppContext } from "../../context/AppContext";
 import { Slider, Header, Producto } from "../index";
 import { WrapperGrid } from "./components";
-import construccion001 from '../../assets/img/imagenes-mastin/construccion001.jpg';
-import construccion002 from '../../assets/img/imagenes-mastin/construccion002.jpg';
-import construccion003 from '../../assets/img/imagenes-mastin/construccion003.jpg';
-import construccion004 from '../../assets/img/imagenes-mastin/construccion004.jpg';
+import construccion001 from '../../assets/img/imagenes-mastin/Construccion001.jpg';
+import construccion002 from '../../assets/img/imagenes-mastin/Construccion002.jpg';
+import construccion003 from '../../assets/img/imagenes-mastin/Construccion003.jpg';
+import construccion004 from '../../assets/img/imagenes-mastin/Construccion004.jpg';
 
-import industria001 from '../../assets/img/imagenes-mastin/industria001.jpg';
-import industria002 from '../../assets/img/imagenes-mastin/industria002.jpg';
-import industria003 from '../../assets/img/imagenes-mastin/industria003.jpg';
-import industria004 from '../../assets/img/imagenes-mastin/industria004.jpg';
+import industria001 from '../../assets/img/imagenes-mastin/Industria001.jpg';
+import industria002 from '../../assets/img/imagenes-mastin/Industria002.jpg';
+import industria003 from '../../assets/img/imagenes-mastin/Industria003.jpg';
+import industria004 from '../../assets/img/imagenes-mastin/Industria004.jpg';
 
-import filtros001 from '../../assets/img/imagenes-mastin/filtros001.jpg';
-import filtros002 from '../../assets/img/imagenes-mastin/filtros002.jpg';
-import filtros003 from '../../assets/img/imagenes-mastin/filtros003.jpg';
-import filtros005 from '../../assets/img/imagenes-mastin/filtros005.jpg';
+import filtros001 from '../../assets/img/imagenes-mastin/Filtros001.jpg';
+import filtros002 from '../../assets/img/imagenes-mastin/Filtros002.jpg';
+import filtros003 from '../../assets/img/imagenes-mastin/Filtros003.jpg';
+import filtros005 from '../../assets/img/imagenes-mastin/Filtros005.jpg';
 
 
 const construccionImg = [
