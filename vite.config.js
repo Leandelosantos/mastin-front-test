29b7bf65be41f25d4887af50b568ca5f53300793
Leandro De los Santos <Cucu@MacBook-Air-de-Leandro.local>
2023-04-27 14:29:13 -0300

    test conection

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: process.env || 3000,
+    host: "0.0.0.0",
+  },
 });
