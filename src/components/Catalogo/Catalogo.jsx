f053eca06555c8bcb6f07faa57eb81f898bd7edc
Leandro De los Santos <Cucu@MacBook-Air-de-Leandro.local>
2023-04-30 21:52:46 -0300

    responsive modifications. I mysql2 to test bbdd

--- src/components/Catalogo/Catalogo.jsx
+++ src/components/Catalogo/Catalogo.jsx
@@ -2,6 +2,7 @@ import { memo, useContext } from "react";
 import { Producto } from "../index";
 import { Title, WrapperGrid } from "./components";
 import { AppContext } from "../../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
 
 const Catalogo = () => {
   const { items } = useContext(AppContext);
@@ -11,7 +12,7 @@ const Catalogo = () => {
       <Title>Catalogo</Title>
       <WrapperGrid>
         {items.map((item) => {
-          return <Producto item={item} />;
+          return <Producto item={item} key={uuidv4()} />;
         })}
       </WrapperGrid>
     </>
