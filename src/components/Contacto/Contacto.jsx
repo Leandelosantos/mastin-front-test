e053fbe2a4a5f7aced555a40e1dd0f93522884a0
Leandelosantos <leandrodelosantos@hotmail.com>
2024-01-24 10:45:02 -0300

    change Arg adress & map

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -7,13 +7,13 @@ const Contacto = () => {
       <WrapperContact>
         <div className="country">
           <h2>Argentina</h2>
-          <p>Dirección: Calle 23 (Anastasio Lanz) 2890.</p>
-          <p>Mini Parque Industrial Vergara, Berazategui.</p>
-          <p>CP1884, Buenos Aires, Argentina.</p>
-          <p>Teléfono: +54 (11) 2120-7200</p>
+          <p>Dirección: Calle 1236 n° 960.</p>
+          <p>Parque Industrial PITEC, Lote 66.</p>
+          <p>Ing. J Allan, Florencio Varela CP 1891, Argentina.</p>
+          <p>Teléfono: +54 (11) 2120-1700</p>
           <p>Lunes a Viernes de 8 a 17 hs</p>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3276.934894010499!2d-58.224304584289456!3d-34.782414280413526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a328c521f03719%3A0xa14649dc34bcd400!2sMas-Tin%20SA!5e0!3m2!1ses!2sar!4v1681164680939!5m2!1ses!2sar"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3274.3299291161243!2d-58.195647724382226!3d-34.84793637286769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2d7ff2e248f6b%3A0x13a6d078d9f675a2!2sPitec%20-%20Parque%20Industrial%20y%20Tecnol%C3%B3gico%20Florencio%20Varela!5e0!3m2!1ses-419!2sar!4v1706103568844!5m2!1ses-419!2sar"
             width="800"
             height="600"
             allowFullScreen=""
